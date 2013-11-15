b61a2575508d9e3b109d2d140294c7f5e987fd8e
arkadiy.sychov <arkadiy.sychov@engagepoint.com>
2013-11-15 15:29:02 +0200

    TB-13 Commit Add Test2

---
+++ src/main/java/Test2.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: arkadiy.sychov
+ * Date: 11/15/13
+ * Time: 3:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test2 {
+}
